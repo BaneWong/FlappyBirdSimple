911651b9d4a587f222898e7608e7e0356380dcb0
ashishsc <ashishsc@u.washington.edu>
2014-03-10 05:49:02 -0700

    Add parallax scrolling

--- SimpleFlappyBird/SimpleFlappyBird/MyScene.m
+++ SimpleFlappyBird/SimpleFlappyBird/MyScene.m
@@ -7,6 +7,8 @@
 //
 
 #import "MyScene.h"
+const float SKYLINE_MOVEMENT_SCALE = 0.01;
+const float GROUND_MOVEMENT_SCALE = 0.02;
 
 @interface MyScene() {
     SKSpriteNode* _bird;
@@ -26,12 +28,18 @@
         SKTexture* groundTexture = [SKTexture textureWithImageNamed:@"Ground"];
         groundTexture.filteringMode = SKTextureFilteringNearest;
         
+        // Ground animation for movement
+        SKAction* moveGroundSprite = [SKAction moveByX:-groundTexture.size.width*2 y:0 duration:GROUND_MOVEMENT_SCALE * groundTexture.size.width*2];
+        SKAction* resetGroundSprite = [SKAction moveByX:groundTexture.size.width*2 y:0 duration:0]; // instantly reset position
+        SKAction* moveGroundSpritesForever = [SKAction repeatActionForever:[SKAction sequence:@[moveGroundSprite, resetGroundSprite]]];
+        
         for (int i = 0; i < 2 + self.frame.size.width / (groundTexture.size.width * 2); ++i) {
 
             // Add some ground sprites
             SKSpriteNode* groundSprite = [SKSpriteNode spriteNodeWithTexture:groundTexture];
             [groundSprite setScale:2.0];
             groundSprite.position = CGPointMake(i * groundSprite.size.width, groundSprite.size.height / 2);
+            [groundSprite runAction:moveGroundSpritesForever];
             [self addChild:groundSprite];
         }
         
@@ -39,11 +47,18 @@
         SKTexture* skylineTexture = [SKTexture textureWithImageNamed:@"Skyline"];
         skylineTexture.filteringMode = SKTextureFilteringNearest;
         
+        // Skyline animation for movement (different scale for parallax effect)
+        SKAction* moveSkylineSprite = [SKAction moveByX:-skylineTexture.size.width*2 y:0 duration:SKYLINE_MOVEMENT_SCALE * skylineTexture.size.width*2];
+        SKAction* resetSkylineSprite = [SKAction moveByX:skylineTexture.size.width*2 y:0 duration:0]; // instantly reset position
+        SKAction* moveSkylineSpritesForever = [SKAction repeatActionForever:[SKAction sequence:@[moveSkylineSprite, resetSkylineSprite]]];
+
+        
         for (int i = 0; i < 2 + self.frame.size.width / (groundTexture.size.width * 2); ++i) {
             SKSpriteNode* skylineSprite = [SKSpriteNode spriteNodeWithTexture:skylineTexture];
             [skylineSprite setScale:2.0];
             skylineSprite.zPosition = -20;
             skylineSprite.position = CGPointMake(i * skylineSprite.size.width, skylineSprite.size.height / 2 + groundTexture.size.height * 2);
+            [skylineSprite runAction:moveSkylineSpritesForever];
             [self addChild:skylineSprite];
         }
         
