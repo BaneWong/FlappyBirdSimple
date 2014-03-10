463ffc1c925ad691e8941c7cc73df915493ecf23
ashishsc <ashishsc@u.washington.edu>
2014-03-10 04:22:17 -0700

    Add bird, init code

--- SimpleFlappyBird/SimpleFlappyBird/MyScene.m
+++ SimpleFlappyBird/SimpleFlappyBird/MyScene.m
@@ -8,42 +8,31 @@
 
 #import "MyScene.h"
 
+@interface MyScene() {
+    SKSpriteNode* _bird;
+}
+@end
+
 @implementation MyScene
 
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
+        SKTexture* birdTexture1 = [SKTexture textureWithImageNamed:@"Bird1"];
+        birdTexture1.filteringMode = SKTextureFilteringNearest;
         
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
-        
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-        
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
-        
-        [self addChild:myLabel];
+        _bird = [SKSpriteNode spriteNodeWithTexture:birdTexture1];
+        [_bird setScale:2.0];
+        _bird.position = CGPointMake(self.frame.size.width / 4, CGRectGetMidY(self.frame));
+        [self addChild:_bird];
     }
     return self;
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
+
     
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
-    }
 }
 
 -(void)update:(CFTimeInterval)currentTime {
